cbc84add47cd4498f37615f9d7710671524fa95c
hyeonjae1122 <hyeonjae+1@gmail.com>
2024-08-12 22:38:27 +0900

    complete to make prototype app

---
+++ lib/ui/widget/photo_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+
+import '../../model/Photo.dart';
+
+class PhotoWidget extends StatelessWidget {
+  final Photo photo;
+
+  const PhotoWidget({Key? key,
+  required this.photo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration( // remove const because of no more const variable in NetworkImage
+          borderRadius: const BorderRadius.all(Radius.circular(16)),
+          image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(
+                photo.previewURL
+              ),)
+      ),
+    );
+  }
+}
