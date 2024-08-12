03deadd3373009f3157734f4ac25c1808618d79b
hyeonjae1122 <hyeonjae+1@gmail.com>
2024-08-12 23:22:19 +0900

    Stream

--- lib/data/photo_provider.dart
+++ lib/data/photo_provider.dart
@@ -1,10 +1,17 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_clean/data/api.dart';
 
+import '../model/Photo.dart';
+
 class PhotoProvider extends InheritedWidget {
   final PixabayApi api;
+  final _photoStreamController = StreamController<List<Photo>>()..add([]);
+  // ..을 사용하면 void를 사용하는 메서드(여기선 add)를 사용하면서 StreamController의 레퍼런스를 반환 할 수 있다.
+  Stream<List<Photo>> get photoStream => _photoStreamController.stream;
 
-  const PhotoProvider({
+  PhotoProvider({
     super.key,
     required this.api,
     required super.child,
@@ -15,6 +22,11 @@ class PhotoProvider extends InheritedWidget {
         context.dependOnInheritedWidgetOfExactType<PhotoProvider>();
     assert(result != null, 'No PixabayApi found in context');
     return result!;
+  }
+
+  Future<void> fetch(String query) async {
+    final result = await api.fetch(query);
+    _photoStreamController.add(result);
   }
 
   @override
--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -13,7 +13,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final _controller = TextEditingController();
-  List<Photo> _photos = [];
+
 
 
   @override
@@ -46,28 +46,36 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                   suffixIcon: IconButton(
                       onPressed: () async {
-                        final photos = await photoProvider.api.fetch(_controller.text);
-                        setState(() {
-                          _photos=photos;
-                        });
+                        photoProvider.fetch(_controller.text);
+
                       }, icon: const Icon(Icons.search)),
                 ),
               ),
             ),
-            Expanded(
-                child: GridView.builder(
-                  padding: const EdgeInsets.all(16.0),
-              itemCount: _photos.length,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-              ),
-              itemBuilder: (context, index) {
-                    final photo = _photos[index];
-                return  PhotoWidget(photo: photo);
-              },
-            ))
+            StreamBuilder<List<Photo>>(
+              stream: photoProvider.photoStream,
+              builder: (context, snapshot) {
+                if(!snapshot.hasData){
+                  return const CircularProgressIndicator();
+                }
+
+                final photos = snapshot.data!;
+                return Expanded(
+                    child: GridView.builder(
+                      padding: const EdgeInsets.all(16.0),
+                  itemCount: photos.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
+                  ),
+                  itemBuilder: (context, index) {
+                        final photo = photos[index];
+                    return  PhotoWidget(photo: photo);
+                  },
+                ));
+              }
+            )
           ],
         ));
   }
