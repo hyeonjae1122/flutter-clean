422a9533e70a371e06b272eda99ae580c51d818d
hyeonjae1122 <hyeonjae+1@gmail.com>
2024-08-13 13:10:33 +0900

    JsonSerializable and Equatable

--- lib/model/photo.dart
+++ lib/model/photo.dart
@@ -1,13 +1,14 @@
+import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
 part 'photo.g.dart';
 
 @JsonSerializable()
-class Photo {
-  int id;
-  String tags;
+class Photo extends Equatable {
+  final int id;
+  final String tags;
 
   @JsonKey(name: 'previewURL')
-  String previewURL;
+  final String previewURL;
 
   Photo({
     required this.id,
@@ -18,4 +19,7 @@ class Photo {
   factory Photo.fromJson(Map<String, dynamic> json) => _$PhotoFromJson(json);
 
   Map<String, dynamic> toJson() => _$PhotoToJson(this);
+
+  @override
+  List<Object?> get props => [id];
 }
